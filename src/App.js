3066614cc58012d9d51c71ca26f1906a069c16a3
Jock Dalby <jock.dalby@citrusad.com>
2022-05-27 15:37:46 +1000

    return to starting app for meetup app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
-import Todo from './components/Todo';
-
 function App() {
   return (
-    <div>
-      <h1>My Todos</h1>
-      <Todo text="Todo 1"/>
-      <Todo text="Todo 2"/>
-      <Todo text="Todo 3"/>
-    </div>
+    <div></div>
   );
 }
 
--- src/components/Backdrop.js
+++
@@ -1,5 +0,0 @@
-function Backdrop(props) {
-  return <div className="backdrop" {...props}/>
-}
-
-export default Backdrop;
