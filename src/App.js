f97b821bf6a538025bbe0415de22902622f41865
Jock Dalby <jock.dalby@citrusad.com>
2022-05-27 15:55:08 +1000

    add routes, Links and MainNavigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
+import { Route, Switch } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
+import MainNavigation from './components/MainNavigation';
+
 function App() {
   return (
-    <div></div>
+    <div>
+      <MainNavigation />
+      {
+        /**
+         * Switch tell the router that only one route should be rendered at a time
+         *  exact prevents the AllMeetupsPage being shown for all routes that
+         * include '/', i.e. all routes
+         **/
+      }
+      <Switch>
+        <Route path='/' exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path='/new-meetup'><NewMeetupPage /></Route>
+        <Route path='/favorites'><FavoritesPage /></Route>
+      </Switch>
+    </div>
   );
 }
 
