26e5e2a0571a45a6f1897a09baaf09f964270525
Jock Dalby <jock.dalby@citrusad.com>
2022-05-27 14:08:37 +1000

    add props to Todo component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo />
+      <Todo text="Todo 1"/>
+      <Todo text="Todo 2"/>
+      <Todo text="Todo 3"/>
     </div>
   );
 }
