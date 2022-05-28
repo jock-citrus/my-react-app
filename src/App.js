38061fa8490a08b4b2c29d310a60412157eb9ce5
Jock Dalby <jock.dalby@citrusad.com>
2022-05-28 13:02:14 +1000

    create and use MeetupList and MeetupItem components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
-import MainNavigation from './components/MainNavigation';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return (
