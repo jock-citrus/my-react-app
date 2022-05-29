75a3dbb3114a2ef90bb38b97c6a2f8ce65cf3e12
Jock Dalby <jock.dalby@citrusad.com>
2022-05-29 11:43:23 +1000

    setup HTTP POST request for adding a new meetup to DB

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,34 @@
+import { useHistory } from 'react-router-dom'
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
-return <section>
-  <h1>Add New Meetup</h1>
-  <NewMeetupForm />
-</section>
+  const history = useHistory();
+
+  function addMeetupHandler(meetupData) {
+    // fetch is a default JS function for sending HTTP requests.
+    fetch('https://react-getting-started-48dec-default-rtdb.firebacseio.com/meetups.json', {
+      // default is 'GET'
+      method: 'POST',
+      body: JSON.stringify(meetupData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(() => {
+      
+    }).catch(err => {
+      console.error(err)
+      // Using push would navigate the user and also allow them to use th back button
+      // to return to current page. This is not what we want in this case, so we use
+      // replace. If user presses back now, they will be returned to page before submitted form.
+      // history.push()
+
+      history.replace('/')
+    });
+  }
+  return (<section>
+    <h1>Add New Meetup</h1>
+    <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+  </section>)
 }
 
 export default NewMeetupPage;
